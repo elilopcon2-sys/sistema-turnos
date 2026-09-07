465ced71ba3a58f479c84bf95bde44c29c2b07ad
Elizabeth Lopez <elilopcon2@gmail.com>
2026-09-07 12:42:28 -0500

    Implementar rutas REST de servicios y configuración del servidor

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
 import "./config/env.config.js";
+import servicesRouter from "./routes/services.router.js";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/services", servicesRouter); 
 
 export default app;
